2107c2379dc06830e3035a18458b678a370f2553
Kye <kye@apacmediasolutions.com>
2024-02-15 11:53:28 -0800

    [README]

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 [![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)
 
 # SwarmOS
-An all-new OS that orchestrates autonomous agents as workers to execute tasks. Inspired by Andrej Karpathy's vision.
+An all-new OS that orchestrates autonomous agents as workers to execute tasks. Inspired by Andrej Karpathy's vision but scaling them up to the thousands of agent OS to create a virtual cloud of agent OSs. We'll be using the Swarms to create the app for it's radical simplicity, bleeding-edge performance, and consistent reliability.
+
+
+## install
+`$ pip install swarmos`
+
+# LLM
+- GPT-4-V
+
+## Tools
+- Calculator
+- Terminal
+- Search API
+- Other LLMs
+
+
+# Disk
+- Gigabind [Text, Vision, Audio, etc]
 
 
 
---
+++ swarmos/main.py
@@ -0,0 +1,73 @@
+import os
+
+from dotenv import load_dotenv
+
+# Import the OpenAIChat model and the Agent struct
+from swarms import Agent, ChromaDB, OpenAIChat, tool
+
+# Load the environment variables
+load_dotenv()
+
+
+# Define a tool
+@tool
+def search_api(query: str, description: str):
+    """Search the web for the query
+
+    Args:
+        query (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return f"Search results for {query}"
+
+
+@tool
+def weather_api(
+    query: str,
+):
+    """_summary_
+
+    Args:
+        query (str): _description_
+    """
+    print(f"Getting the weather for {query}")
+
+
+@tool
+def rapid_api(query: str):
+    """_summary_
+
+    Args:
+        query (str): _description_
+    """
+    print(f"Getting the weather for {query}")
+
+
+# Get the API key from the environment
+api_key = os.environ.get("OPENAI_API_KEY")
+
+# Initialize the language model
+llm = OpenAIChat(
+    temperature=0.5,
+    openai_api_key=api_key,
+)
+
+
+## Initialize the workflow
+agent = Agent(
+    agent_name="SwarmOS-Worker",
+    agent_description=(
+        "A worker agent for SwarmOS, the LLM-based Operating System."
+    ),
+    llm=llm,
+    max_loops="auto",
+    dashboard=True,
+    tools=[search_api, weather_api, rapid_api],
+    long_term_memory=ChromaDB(),
+)
+
+# Run the workflow on a task
+out = agent.run("Generate a 10,000 word blog on health and wellness.")
+print(out)
